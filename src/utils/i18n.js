e59e0ece67cdb1bcd1353676647e855bf43754d8
lasha-zumbadze <lashazumbadze6@gmail.com>
2025-03-18 04:41:43 -0700

    add and config i18n framework, create separate json files for two languages

---
+++ src/utils/i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "../assets/languages/en.json";
+import ge from "../assets/languages/ge.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  fr: {
+    translation: ge,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
